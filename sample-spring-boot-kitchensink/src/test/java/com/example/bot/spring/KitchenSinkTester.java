1c44955e8aeacc5a7e0757669c7cc1664cf766a2
404akhan <404akhan@gmail.com>
2017-09-18 19:12:51 +0800

    test contain

--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
@@ -87,4 +87,17 @@ public class KitchenSinkTester {
 		assertThat(!thrown);
 		assertThat(result.equals("Hey, how things going?"));
 	}
+	
+	@Test
+	public void testContains() throws Exception {
+		boolean thrown = false;
+		String result = null;
+		try {
+			result = this.databaseEngine.search("Do you know what comes after abc?");
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown);
+		assertThat(result.equals("def"));
+	}
 }
